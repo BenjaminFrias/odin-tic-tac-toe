d40651cdee28f2db3726cefb976bdf123b5e7384
BenjaminFrias <benjaminfrias2004@gmail.com>
2024-05-27 18:01:17 -0400

    add formmated board

--- script.js
+++ script.js
@@ -11,30 +11,98 @@ function Gameboard() {
         }
     }
 
-    const addMark = function (player, row, column) {
+    const addMark = function (mark, row, column) {
         const cell = board[row][column];
-        // Check for available spaces
-        if (cell.getValueCell() !== 0) {
+        // Check if it is available
+        if (cell.getCellValue() !== 0) {
             return;
         }
 
-        cell.changeCellValue(player.mark);
-    }
+        cell.changeCellValue(mark);
+        return cell;
+    };
 
-    return { board, addMark }
+    const getBoard = () => {
+        return board;
+    };
+
+    const getFormattedBoard = () => {
+        let marksArr = [];
+        let formmatedBoard = "";
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                marksArr.push(board[i][j].getCellValue());
+            }
+        }
+
+        for (let i = 0; i < 9; i++) {
+            if (i % 3 == 0 && i > 0) {
+                formmatedBoard += "\n";
+            }
+            formmatedBoard += ` ${marksArr[i]}`;
+        }
+
+        return formmatedBoard;
+
+    };
+
+    return { getBoard, addMark, getFormattedBoard };
 }
 
 // Individual cell from the board
 function Cell() {
     let value = 0;
 
+    const getCellValue = () => {
+        return value;
+    };
+
     const changeCellValue = function (mark) {
         value = mark;
     };
 
-    return { value, changeCellValue };
+    return { getCellValue, changeCellValue };
 }
 
 // Control the flow of the game
+function GameController(name1 = "Player1", name2 = "Player2") {
+    // Create players object
+    const players = {
+        player1: {
+            name: name1,
+            mark: 1,
+        },
+        player2: {
+            name: name2,
+            mark: 2,
+        },
+    };
 
-Gameboard();
+    const gameBoard = Gameboard();
+    let activePlayer = players.player1;
+
+    const switchTurn = () => {
+        activePlayer == players.player1
+            ? (activePlayer = players.player2)
+            : (activePlayer = players.player1);
+    };
+
+    const playRound = () => {
+        console.log(`${activePlayer.name}'s turn...`);
+
+        let row = prompt("Select a row (0 - 2)");
+        let col = prompt("Select a column (0 - 2)");
+
+        if (!gameBoard.addMark(activePlayer.mark, row, col)) {
+        }
+
+        console.log(gameBoard.getFormattedBoard());
+        switchTurn();
+    };
+
+    return { playRound, switchTurn, players };
+}
+
+const game = GameController();
+game.playRound();
+game.playRound();
